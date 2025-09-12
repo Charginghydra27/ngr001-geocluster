07cb6c8aff751a60fffa899eba87b879840569b2
Noah John <ngaudineer@unomaha.edu>
2025-09-12 12:52:32 -0500

    Map UI with H3 Aggregation; data seeding and API working

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+﻿from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, DateTime, JSON, Float
+from datetime import datetime  # Python type (IMPORTANT)
 
 class Base(DeclarativeBase):
     pass
@@ -7,7 +8,7 @@ class Base(DeclarativeBase):
 class Event(Base):
     __tablename__ = "events"
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    occurred_at: Mapped[DateTime]
+    occurred_at: Mapped[datetime] = mapped_column(DateTime)  # use Python datetime here
     lat: Mapped[float] = mapped_column(Float)
     lon: Mapped[float] = mapped_column(Float)
     type: Mapped[str | None] = mapped_column(String, nullable=True)
