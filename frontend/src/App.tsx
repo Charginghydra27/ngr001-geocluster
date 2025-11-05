63315859f0bd15cf96660c15c31fa9c07a01b7b5
Carlos Jordan <151295984+Carlos3443@users.noreply.github.com>
2025-11-04 20:31:04 -0600

    Delete frontend directory

--- frontend/src/App.tsx
+++
@@ -1,9 +0,0 @@
-﻿import MapView from "./map/MapView";
-export default function App() {
-  return (
-    <div style={{ height: "100vh", width: "100vw" }}>
-      <MapView />
-      <div style={{position:"absolute", bottom:10, left:10, background:"#fff"}}>hello</div>
-    </div>
-  );
-}
--- frontend/src/config.ts
+++
@@ -1,4 +0,0 @@
-export const MAPTILER_KEY =
-  import.meta.env.VITE_MAPTILER_KEY ?? "B4z2zRsFpp2XMst54fhA";
-export const API_BASE =
-  import.meta.env.VITE_API_URL ?? "http://localhost:8000";
